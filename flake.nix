20904464170d1ca6772cf80bb2935abcfe20465a
Gerhard Gustavsen <gerhard.gustavsen@outlook.com>
2025-09-19 00:46:43 +0200

    2025-09-19 00:46:43

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Astro project dev shell with Node + pnpm";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            nodejs_22 # or nodejs_20 if you want LTS
+            pnpm
+          ];
+
+          shellHook = ''
+            echo "🚀 Entered Astro dev shell (Node: $(node -v), pnpm: $(pnpm -v))"
+          '';
+        };
+      });
+}
+
